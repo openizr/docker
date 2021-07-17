9014332cd589e819e00e36680433af8d276b66a4
Matthieu Jabbour <matthieu.jabbour@gmail.com>
2021-07-17 20:35:07 +0200

    Update node version, add register_ssh script, update documentation

--- node-dev/docker-entrypoint.sh
+++ node-dev/docker-entrypoint.sh
@@ -2,16 +2,7 @@
 set -e
 
 # Registering SSH keys if needed...
-if [ ! -z "${SSH_PRIVATE_KEY:-}" ] && [ ! -z "${SSH_PUBLIC_KEY:-}" ]; then
-  echo -e $SSH_PRIVATE_KEY > $HOME/.ssh/id_rsa
-  chmod 600 $HOME/.ssh/id_rsa
-  echo -e $SSH_PUBLIC_KEY > $HOME/.ssh/id_rsa.pub
-  chmod 644 $HOME/.ssh/id_rsa.pub
-  ssh-keyscan -H 'github.com' > $HOME/.ssh/known_hosts 2> /dev/null
-  chmod 644 $HOME/.ssh/known_hosts
-  chmod 700 $HOME/.ssh
-  echo "Successfully registered SSH keys"
-fi
+register_ssh
 
 # Default node entrypoint.
 if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
